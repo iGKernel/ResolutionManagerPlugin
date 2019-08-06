9bd6d57276d7629e627ae08cbf984b1d910bc3bc
redApple91 <mahmoud.mheisen@gmail.com>
2019-08-06 16:58:03 +0300

    split plugin setting code from functionality

--- addons/ResolutionSwitcher/CustomResWindow.gd
+++ addons/ResolutionSwitcher/CustomResWindow.gd
@@ -1,27 +1,31 @@
+tool
 extends WindowDialog
 
-#const CUSTOM_LIST_FILE_PATH: String = "res://addons/ResolutionSwitcher/list_custom.txt";
-#
-#signal reload;
-## Config file to load resolution list file:
-#var config_file: ConfigFile = null;
-#
-#
-#func _ready():
-#	config_file = ConfigFile.new();
-#
-#func _on_ok_pressed():
-#	var width: String = String(find_node("width").text);
-#	var height: String = String(find_node("height").text);
-#	var label: String = String(find_node("labelv").text);
-#
-#	var text : String = width + "x" + height;
-#
-#	config_file.set_value("Custom Resolutions", label, text);
-#	config_file.save(CUSTOM_LIST_FILE_PATH);
-#	get_parent().load_resolution_list();
-#	hide();
-#
-#
-#func _on_cancel_pressed():
-#	hide();
+const CUSTOM_LIST_FILE_PATH: String = "res://addons/ResolutionSwitcher/lists/list_custom.txt";
+
+signal reload;
+
+# Config file to load resolution list file:
+var config_file: ConfigFile = null;
+
+
+func _ready():
+	config_file = ConfigFile.new();
+
+func _on_ok_pressed():
+	config_file.load(CUSTOM_LIST_FILE_PATH)
+	var width: String = String(find_node("width").text);
+	var height: String = String(find_node("height").text);
+	var label: String = String(find_node("labelv").text);
+
+	var text : String = width + "x" + height;
+
+	config_file.set_value("Custom Resolutions", label, text);
+	config_file.save(CUSTOM_LIST_FILE_PATH);
+	emit_signal("reload");
+	
+	hide();
+
+
+func _on_cancel_pressed():
+	hide();
--- addons/ResolutionSwitcher/ResolutionButton.gd
+++ addons/ResolutionSwitcher/ResolutionButton.gd
@@ -1,13 +1,292 @@
+tool
 extends MenuButton
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Text file contain list of pre-defined resolutions:
+const RESOLUTION_LIST_FILE_PATH: String = "res://addons/ResolutionSwitcher/lists/list.txt";
+const LARGE_LIST_FILE_PATH: String = "res://addons/ResolutionSwitcher/lists/list_large.txt";
+const IPHONE_LIST_FILE_PATH: String = "res://addons/ResolutionSwitcher/lists/list_iphone.txt";
+const MOSTUSED_LIST_FILE_PATH: String = "res://addons/ResolutionSwitcher/lists/list_mostused.txt";
+const CUSTOM_LIST_FILE_PATH: String = "res://addons/ResolutionSwitcher/lists/list_custom.txt";
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+enum ListsPath {
+	BASIC, LARGE, IPHONE, MOSTUSED, CUSTOM
+};
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+var current_list: String = RESOLUTION_LIST_FILE_PATH;
+var json_dict: Dictionary = {};
+var res_list_id: int = 0;
+var first: bool = true;
+var last_list: int = 0;
+var last_stretch: int = 0;
+
+# Canvas editor menu button and popup:
+var menu_popup: PopupMenu  = null;
+var set_res_window = null
+var custom_res_window = null
+var stretch_settings_submenu: PopupMenu = null;
+var list_submenu: PopupMenu = null;
+
+# Config file to load resolution list file:
+var config_file: ConfigFile = null;
+
+# Dictionary to hold the width and height of clicked resolution:
+var resolution_data: Dictionary = {};
+
+
+func _enter_tree()-> void:
+	var file: File = File.new();
+	file.open("res://addons/ResolutionSwitcher/stretch_settings_tooltip.json", file.READ);
+	json_dict = parse_json(file.get_as_text());
+	file.close()
+	
+	# Connect id_pressed signal to switch resloution:
+	menu_popup = get_popup();
+	menu_popup.connect("id_pressed", self, "_on_menu_popup_id_pressed");
+	
+	set_res_window = preload("set_res_window.tscn").instance();
+	custom_res_window = preload("CustomResWindow.tscn").instance();
+	
+	# Fill popup menu and resolution data dictionary:
+	load_resolution_list();
+	custom_res_window.connect("reload", self, "load_resolution_list")
+
+
+func _exit_tree()-> void:
+	# Clear popup menu and resolution data dictionary:
+	resolution_data.clear();
+	menu_popup.clear();
+	
+	# Disconnect id_pressed signal:
+	menu_popup.disconnect("id_pressed", self, "_on_menu_popup_id_pressed");
+	
+	# Free menu button and popup:
+	menu_popup.queue_free();
+
+
+# Fill popup menu and resolution data dictionary:
+func load_resolution_list()-> void:
+	config_file = ConfigFile.new();
+	var is_loaded: = config_file.load(current_list);
+	if is_loaded != OK:
+		return;
+	
+	resolution_data = {};
+	menu_popup.clear();
+
+	load_stretch_settings_submenu();
+	load_list_submenu();
+	
+	menu_popup.add_item("Set Base Resolution");
+	menu_popup.add_item("Add Custom Resolution");
+	menu_popup.add_separator();
+	menu_popup.add_item("Test Resolutions:");
+	menu_popup.set_item_disabled(5, true);
+	
+	if res_list_id == 4:
+		menu_popup.set_item_disabled(3, false);
+	else:
+		menu_popup.set_item_disabled(3, true);
+	
+	var node = set_res_window.find_node("OptionButton");
+	if first:
+		node.connect("item_selected", self, "_on_node_item_selected");
+		first = false;
+	node.clear();
+	
+	# Fill data:
+	var sections: PoolStringArray = config_file.get_sections();
+	
+	for section in sections:
+		var keys: PoolStringArray = config_file.get_section_keys(section);
+		menu_popup.add_separator(section);
+		node.add_separator();
+		
+		for key in keys:
+			# Split at "x":
+			var value = config_file.get_value(section,key).split("x");
+			var width = value[0];
+			var height = value[1];
+			var text = key + "    (" + width + "x" + height +")";
+			
+			resolution_data[text] = {
+				"label": key,
+				"width": width,
+				"height": height
+			};
+			
+			menu_popup.add_item(text);
+			node.add_item(text);
+
+
+# Create stretch settings submenu:
+func load_stretch_settings_submenu()-> void:
+	if stretch_settings_submenu:
+		menu_popup.remove_child(stretch_settings_submenu);
+		stretch_settings_submenu.clear();
+		stretch_settings_submenu.queue_free();
+		
+	stretch_settings_submenu = PopupMenu.new();
+	stretch_settings_submenu.name = "stretch_settings"; # used in add_submenu_item function
+	stretch_settings_submenu.connect("id_pressed", self, "_on_stretch_settings_submenu_id_pressed");
+	
+	var array: Array =["Screen Fill: 2d, ignored", "One Ratio: 2d, keep",
+	"GUI/Vertical: 2d, keep_width", "Horizontal platformer: 2d, keep_height", "Expand: 2d, expand"];
+	
+	var text: String = "Full Control: disable, ignored";
+	for i in range(11):
+		if i != 0 and i < 6:
+			text = array[i-1]
+		elif i >= 6:
+			text = "Pixel-Perfect, " + array[fmod(i-1, 5)];
+		
+		stretch_settings_submenu.add_radio_check_item(text, i);
+		stretch_settings_submenu.set_item_tooltip(i, json_dict[String(i)]);
+	
+	update_radio_group_check_state(stretch_settings_submenu, last_stretch);
+	
+	menu_popup.add_child(stretch_settings_submenu);
+	menu_popup.add_submenu_item("Stretch Settings", "stretch_settings");
+
+
+func load_list_submenu()-> void:
+	if list_submenu:
+		menu_popup.remove_child(list_submenu);
+		list_submenu.clear();
+		list_submenu.queue_free();
+	
+	list_submenu = PopupMenu.new();
+	list_submenu.name = "list_submenu"; # used in add_submenu_item function
+	list_submenu.connect("id_pressed", self, "_on_list_submenu_id_pressed");
+
+	list_submenu.add_radio_check_item("Basic List", 0);
+	list_submenu.add_radio_check_item("Large List", 1);
+	list_submenu.add_radio_check_item("IPhone List", 2);
+	list_submenu.add_radio_check_item("Most Used List", 3);
+	list_submenu.add_radio_check_item("Custom List", 4);
+
+	update_radio_group_check_state(list_submenu, last_list);
+	
+	menu_popup.add_child(list_submenu);
+	menu_popup.add_submenu_item("Resolution List", "list_submenu");
+
+
+func update_radio_group_check_state(menu: PopupMenu, idx: int)-> void:
+	var item_count: int = menu.get_item_count();
+	for i in range(item_count):
+		if i == idx:
+			if not menu.is_item_checked(i):
+				menu.toggle_item_checked(i);
+		else:
+			if menu.is_item_checked(i):
+				menu.toggle_item_checked(i);
+
+
+func set_res_logic()-> void:
+	if set_res_window.get_parent() == null:
+		add_child(set_res_window);
+	
+	set_res_window.find_node("OptionButton").text = "Choose pre-defined resolution";
+	set_res_window.find_node("width").text = "";
+	set_res_window.find_node("height").text = "";
+	set_res_window.find_node("width").placeholder_text = "Enter Width";
+	set_res_window.find_node("height").placeholder_text = "Enter Height";
+	var current_size: Vector2 = Vector2();
+	current_size.x = ProjectSettings.get_setting("display/window/size/width");
+	current_size.y = ProjectSettings.get_setting("display/window/size/height");
+	set_res_window.find_node("current").text = "    Current resolution: " + String(current_size.x) + " x " + String(current_size.y);
+
+	set_res_window.show();
+	set_res_window.popup_centered();
+	set_res_window.find_node("ok").connect("pressed", self, "_on_ok", [], CONNECT_ONESHOT);
+	set_res_window.find_node("cancel").connect("pressed", self, "_on_cancel", [], CONNECT_ONESHOT);
+
+
+func custom_res_logic()-> void:
+	if custom_res_window.get_parent() == null:
+		add_child(custom_res_window);
+	
+	#custom_res_window.connect("reload", self, "load_resolution_list");	
+	custom_res_window.show();
+	custom_res_window.popup_centered();
+	#custom_res_window.find_node("ok").connect("pressed", self, "_on_ok2", [], CONNECT_ONESHOT);
+	#custom_res_window.find_node("cancel").connect("pressed", self, "_on_cancel2", [], CONNECT_ONESHOT);
+
+
+func _on_menu_popup_id_pressed(id: int)-> void:
+	var key := menu_popup.get_item_text(id);
+	if key == "Set Base Resolution":
+		set_res_logic();
+	elif key == "Add Custom Resolution":
+		custom_res_logic();
+	else:
+		var width: int = resolution_data[key]["width"];
+		var height: int = resolution_data[key]["height"];
+		
+		text = key;
+		
+		ProjectSettings.set_setting("display/window/size/test_width", width);
+		ProjectSettings.set_setting("display/window/size/test_height", height);
+		ProjectSettings.save();
+
+
+func _on_node_item_selected(id: int)-> void:
+	var key = set_res_window.find_node("OptionButton").get_item_text(id);
+#	var idx = set_res_window.find_node("OptionButton").get_item_index(id);
+#	set_res_window.find_node("OptionButton").select(idx);
+	
+	var width: int = resolution_data[key]["width"];
+	var height: int = resolution_data[key]["height"];
+	
+	set_res_window.find_node("width").text = String(width);
+	set_res_window.find_node("height").text = String(height);
+
+
+func _on_stretch_settings_submenu_id_pressed(id: int)-> void:
+	var array: Array = ["ignore", "keep", "keep_width", "keep_height", "expand"];
+	var mode = "disabled";
+	var aspect = "ignore";
+	if id != 0 and id < 6:
+		mode = "2d";
+		aspect = array[id-1];
+	elif id >= 6:
+		mode = "viewport";
+		aspect = array[fmod(id-1, 5)];
+
+	update_radio_group_check_state(stretch_settings_submenu, id);
+	ProjectSettings.set_setting("display/window/stretch/mode", mode);
+	ProjectSettings.set_setting("display/window/stretch/aspect", aspect);
+	last_stretch = id;
+
+
+func _on_list_submenu_id_pressed(id: int)-> void:
+	var idx = list_submenu.get_item_index(id);
+	update_radio_group_check_state(list_submenu, idx);
+	res_list_id = idx;
+	if idx == 0:
+		current_list = RESOLUTION_LIST_FILE_PATH;
+	elif idx == 1:
+		current_list = LARGE_LIST_FILE_PATH;
+	elif idx == 2:
+		current_list = IPHONE_LIST_FILE_PATH;
+	elif idx == 3:
+		current_list = MOSTUSED_LIST_FILE_PATH;
+	elif idx == 4:
+		current_list = CUSTOM_LIST_FILE_PATH;
+	
+	last_list = idx;
+	load_resolution_list();
+
+
+func _on_ok()-> void:
+	var width: int = int(set_res_window.find_node("width").text);
+	var height: int = int(set_res_window.find_node("height").text);
+	ProjectSettings.set_setting("display/window/size/width", width);
+	ProjectSettings.set_setting("display/window/size/height", height);
+	ProjectSettings.save();
+	set_res_window.hide();
+
+
+func _on_cancel()-> void:
+	set_res_window.hide();
+
+
